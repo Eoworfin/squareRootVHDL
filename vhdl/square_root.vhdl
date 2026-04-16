be32184e31b5f53fdac88e2304e9d6351e7d3933
sdirni <s.dirnberger20@gmail.com>
2026-04-16 09:02:34 +0200

    neue version mit FSM-D

--- vhdl/square_root.vhdl
+++ vhdl/square_root.vhdl
@@ -11,104 +11,100 @@ use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
 entity squareRoot is
-    port (
-        clock   : in  std_logic;
-        reset   : in  std_logic;
-        start   : in  std_logic;
-        value   : in  std_logic_vector(9 downto 0);
-        roundup : in  std_logic;
-        done    : out std_logic;
-        result  : out std_logic_vector(9 downto 0)
-    );
-end entity squareRoot;
+port (
+    clock   : in std_logic;
+    reset   : in std_logic;
+    start   : in std_logic;
+    value   : in std_logic_vector(9 downto 0);
+    roundup : in std_logic;
+    done    : out std_logic;
+    result  : out std_logic_vector(9 downto 0)
+);
+end entity;
+
 
 architecture rtl of squareRoot is
-
-    type state_type is (IDLE, INIT, CALC, FINISH);
-    signal state : state_type;
-
-    signal root_reg      : unsigned(9 downto 0);
-    signal remainder_reg : unsigned(9 downto 0);
-    signal mask_reg      : unsigned(9 downto 0);
-
-    signal done_reg      : std_logic; 
-    signal result_reg    : unsigned(9 downto 0); 
-
-    constant NR_OF_BITS : integer := 10;
+    type state_type is (IDLE, INIT, CHECK, UPDATE, SHIFT, ROUND, DONE);
+    signal s_state, s_next_state : s_state_type;
+    signal s_root      : unsigned(9 downto 0);
+    signal s_remainder : unsigned(9 downto 0);
+    signal s_mask      : unsigned(9 downto 0);
+    signal s_root_next, s_remainder_next, s_mask_next : unsigned(9 downto 0);
 
 begin
 
-    done   <= done_reg;
-    result <= std_logic_vector(result_reg);
+-- Flip Flop
+process(clock, reset)
+begin
+    if reset = '1' then
+        s_state <= IDLE;
+    elsif rising_edge(clock) then
+        s_state <= s_next_state;
+        s_root <= s_root_next;
+        s_remainder <= s_remainder_next;
+        s_mask <= s_mask_next;
+    end if;
+end process;
 
-    process(clock, reset)
-        variable root_temp      : unsigned(9 downto 0);
-        variable remainder_temp : unsigned(9 downto 0);
-    begin
-        if reset = '1' then
-            state         <= IDLE;
-            done_reg      <= '0';
-            result_reg    <= (others => '0');
-            root_reg      <= (others => '0');
-            remainder_reg <= (others => '0');
-            mask_reg      <= (others => '0');
 
-        elsif rising_edge(clock) then
+-- Transition Logic
+process(s_state, start, s_mask, s_remainder, s_root, roundup)
+begin
+    s_next_state <= s_state;
 
-            done_reg <= '0';
+    case s_state is
 
-            case state is
+        when IDLE => if start = '1' then
+                s_next_state <= INIT;
+            end if;
 
-                when IDLE =>
-                    if start = '1' then
-                        remainder_reg <= unsigned(value);
-                        state         <= INIT;
-                    end if;
+        when INIT => s_next_state <= CHECK;
 
-                when INIT =>
-                    root_reg      <= (others => '0');
-                    mask_reg      <= to_unsigned(1, 10) sll (NR_OF_BITS - 2);  -- 2^(8) = 256 bei 10 Bit
-                    state         <= CALC;
-
-                when CALC =>
-                    root_temp      := root_reg;
-                    remainder_temp := remainder_reg;
-
-                    -- if ((root + mask) <= remainder)
-                    if (root_temp + mask_reg) <= remainder_temp then
-                        remainder_temp := remainder_temp - (root_temp + mask_reg);
-                        root_temp      := root_temp + (mask_reg sll 1);   -- root += (mask << 1)
-                    end if;
-
-                    -- root >>= 1
-                    root_temp := root_temp srl 1;
-
-                    -- mask >>= 2
-                    mask_reg <= mask_reg srl 2;
-
-                    -- Update Register
-                    root_reg      <= root_temp;
-                    remainder_reg <= remainder_temp;
-
-                    -- Schleife beenden, wenn mask nach Shift == 0
-                    if (mask_reg srl 2) = 0 then
-                        state <= FINISH;
-                    end if;
-
-                    when FINISH =>
-                    if (roundup = '1') and (remainder_reg > root_reg) then
-                        result_reg <= root_reg + 1;
+        when CHECK => if s_mask = 0 then
+                s_next_state <= ROUND;
                     else
-                        result_reg <= root_reg;
-                    end if;
+                s_next_state <= UPDATE;
+            end if;
+        when UPDATE => s_next_state <= SHIFT;
+        when SHIFT => s_next_state <= CHECK;
+        when ROUND => s_next_state <= DONE;
+        when DONE => s_next_state <= IDLE;
+    end case;
+end process;
 
-                    done_reg <= '1';
-                    state    <= IDLE;
 
-                when others => state <= IDLE;
+-- Datapath Logic
+process(s_state, value, s_root, s_remainder, s_mask, roundup)
+begin
+    s_root_next <= s_root;
+    s_remainder_next <= s_remainder;
+    s_mask_next <= s_mask;
+    done <= '0';
 
-            end case;
-        end if;
-    end process;
+    case s_state is
+        when IDLE => null;
+        when INIT =>
+            s_root_next <= (others => '0');
+            s_remainder_next <= unsigned(value);
+            s_ mask_next <= to_unsigned(256, 10); -- 2^(10-2)
+        when CHECK => null;
+        when UPDATE =>
+            if (s_root + s_mask) <= s_remainder then
+                s_remainder_next <= s_remainder - (s_root + s_mask);
+                s_root_next <= s_root + (s_mask sll 1);
+            end if;
+        when SHIFT =>
+            s_root_next <= s_root_next srl 1;
+            s_mask_next <= s_mask srl 2;
+        when ROUND =>
+            if (s_remainder > s_root) and (s roundup = '1') then
+                s_root_next <= s_root + 1;
+            end if;
+        when DONE => done <= '1';
+    end case;
+end process;
 
-end architecture rtl;
+--Output
+result <= std_logic_vector(s_root);
+
+end architecture;
